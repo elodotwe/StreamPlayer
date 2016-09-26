cc0678eb3d2578affa939eb59daead2b63754609
Jacob Rau <jarau@mtu.edu>
2016-09-26 17:52:39 -0400

    Basic AppLink functionality working with ALE, that's it.

---
+++ app/src/main/java/com/jacobarau/streamplayer/MainActivity.java
@@ -0,0 +1,24 @@
+package com.jacobarau.streamplayer;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        SdlService serviceInstance = SdlService.getInstance();
+        if (serviceInstance == null) {
+            Intent startIntent = new Intent(this.getApplicationContext(), SdlService.class);
+            Intent bsIntent = new Intent();
+            startIntent.putExtras(bsIntent);
+            this.getApplicationContext().startService(startIntent);
+        }
+
+
+    }
+}
